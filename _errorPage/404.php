17de5222326f2dce3f749de666a67be470315236
nankonyo <nankonyo@gmail.com>
2025-03-14 17:33:28 +0700

    add style aja

--- _errorPage/404.php
+++ _errorPage/404.php
@@ -1,1 +1,33 @@
-404 - page not found.
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>404 | Page not Found</title>
+	<meta name="theme-color" content="#d9230f" media="(prefers-color-scheme: light)">
+	<link href="/lib/bootswatch/5.3/css/spacelab.css" rel="stylesheet">
+	<link href="/lib/bootsrap-icons/1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+	<link rel="icon" type="image/x-icon" href="/assets/img/icon.png">
+	<script src="/lib/jquery/jquery-3.7.1/js/jquery-3.7.1.min.js"></script>
+	<noscript>
+	     <meta http-equiv="refresh" content="0;url=redirect/nojs?redi=">
+	</noscript>
+</head>
+
+<body>
+	<main style="margin-top:5rem;">
+		<div class="container-fluid" style="max-width: 500px;">
+			<div class="card p-5 border-0">
+				<h4 class="fw-bold text-primary mb-3">
+					<i class="bi bi-file-earmark-x"></i> 404 | Page not Found
+				</h4>
+				<p class="text-muted" style="font-size:0.8rem;">
+					Sorry, the page you are looking for does not exist. Please check the URL again or return to the homepage.
+				</p>
+				<a href="/" class="text-end text-decoration-none mt-3 fw-bold"><i class="bi bi-arrow-return-right"></i> homePage</a>
+			</div>
+		</div>
+	</main>
+	<script src="/lib/bootstrap/5.3/js/bootstrap.bundle.js"></script>
+</body>
+</html>
